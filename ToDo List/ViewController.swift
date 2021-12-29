d2936627bcb96394b2043f2ec8c0b7b7cc4d1f9f
Carlos Alcala <96314148+Kratos1976@users.noreply.github.com>
2021-12-29 19:33:27 -0400

    Update ViewController.swift

--- ToDo List/ViewController.swift
+++ ToDo List/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    let a: String = "This is a Test"
+    let a: String = "This is a super "
 
     override func viewDidLoad() {
         super.viewDidLoad()
